17747d6195d8f780f09b81e44921dc41b75b29d0
Mike <mikesantia.6297@gmail.com>
2019-12-16 22:14:23 +0700

    [initial] project

---
+++ src/main/java/com/enigma/MangTixApplication.java
@@ -0,0 +1,13 @@
+package com.enigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangTixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangTixApplication.class, args);
+    }
+
+}
