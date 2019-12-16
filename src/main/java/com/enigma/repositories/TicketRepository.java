f21abefeb18afb43fbb016d605c2c424d120563f
Mike <mikesantia.6297@gmail.com>
2019-12-16 23:16:10 +0700

    [FEATURE] entity Ticket and Category

--- src/main/java/com/enigma/repositories/TicketRepository.java
+++ src/main/java/com/enigma/repositories/TicketRepository.java
@@ -1,4 +1,7 @@
 package com.enigma.repositories;
 
-public class TicketRepository {
+import com.enigma.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketRepository extends JpaRepository<Ticket, String> {
 }
