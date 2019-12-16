8f2155b239f8cc7600ef9e33f236b48688e7a306
fatiri <ilhamfatiriblog@gmail.com>
2019-12-17 00:36:04 +0700

    [FEATURE] add UserController

--- src/main/java/com/enigma/services/UserService.java
+++ src/main/java/com/enigma/services/UserService.java
@@ -1,9 +1,10 @@
 package com.enigma.services;
 
 import com.enigma.entity.User;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public interface UserService {
 
     public User saveUser(User user);
