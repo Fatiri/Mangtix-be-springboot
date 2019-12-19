4f58706741d0e2a665c9e3426678adb5f9c1c859
Ragil Arain Sawala <arainsyawala@gmail.com>
2019-12-20 01:08:47 +0700

    [IMPROVE] making constanta for relation to company

--- src/main/java/com/enigma/constanta/EventConstanta.java
+++ src/main/java/com/enigma/constanta/EventConstanta.java
@@ -9,6 +9,7 @@ public class EventConstanta {
     public static final String EVENT_DATE_PATTERN = "dd-MM-yyyy";
     public static final String EVENT_ID_RELATION = "event_id";
     public static final String LOCATION_ID_RELATION = "location_id";
+    public static final String COMPANY_ID_RELATION = "company_id";
     public static final String EVENT_MAPPED_BY = "event";
     public static final String FILE_DIRECTORY = "/var/www/html/image/";
 
