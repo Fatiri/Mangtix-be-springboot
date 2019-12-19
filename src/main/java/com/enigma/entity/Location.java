18df3844a7ae3234b945d736b8f1443eeecbc91b
Mike <mikesantia.6297@gmail.com>
2019-12-19 10:02:07 +0700

    [IMPRUVE][FEATURE] entity Location

--- src/main/java/com/enigma/entity/Location.java
+++ src/main/java/com/enigma/entity/Location.java
@@ -1,4 +1,80 @@
 package com.enigma.entity;
 
+import com.enigma.constanta.StringConstant;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = StringConstant.MST_LOCATION)
 public class Location {
+    @Id
+    @GeneratedValue(generator = StringConstant.SYSTEM_UUID2)
+    @GenericGenerator(name = StringConstant.SYSTEM_UUID2, strategy = StringConstant.UUID2)
+    private String id;
+    private String city;
+    private String venue;
+    private String address;
+
+    public Location() {
+
+    }
+
+    public Location(String city, String venue, String address) {
+        this.city = city;
+        this.venue = venue;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return Objects.equals(id, location.id) &&
+                Objects.equals(city, location.city) &&
+                Objects.equals(venue, location.venue) &&
+                Objects.equals(address, location.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, city, venue, address);
+    }
 }
