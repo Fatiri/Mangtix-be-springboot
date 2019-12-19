f75f40dc2a039dc54913a59faa1f5775dfb6a3d9
Mike <mikesantia.6297@gmail.com>
2019-12-19 10:05:02 +0700

    [IMPRUVE][FEATURE] service LocationServiceImpl

--- src/main/java/com/enigma/services/impl/LocationServiceImpl.java
+++ src/main/java/com/enigma/services/impl/LocationServiceImpl.java
@@ -1,4 +1,36 @@
 package com.enigma.services.impl;
 
+import com.enigma.constanta.MessageConstant;
+import com.enigma.entity.Location;
+import com.enigma.exception.NotFoundException;
+import com.enigma.repositories.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class LocationServiceImpl {
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    public Location saveLocation(Location location){
+        return locationRepository.save(location);
+    }
+
+    public Location getLocationById(String id){
+        if (!locationRepository.findById(id).isPresent()){
+            throw new NotFoundException(String.format(MessageConstant.ID_LOCATION_NOT_FOUND, id));
+        }
+        return locationRepository.findById(id).get();
+    }
+
+    public List<Location> getAllLocation(){
+        return locationRepository.findAll();
+    }
+
+    public void deleteLocationById(String id){
+        locationRepository.deleteById(id);
+    }
 }
