d34c2ab2aa2d7bb494bf5c18a09ee4323a804915
Nopalina Ayu Munthe <nopalinaayumunthe02@gmail.com>
2019-12-20 00:38:25 +0700

    [FEATURE] service company

--- src/main/java/com/enigma/services/CompanyService.java
+++ src/main/java/com/enigma/services/CompanyService.java
@@ -1,4 +1,15 @@
 package com.enigma.services;
 
+import com.enigma.entity.Company;
+
+import java.util.List;
+
 public interface CompanyService {
+    Company saveCompany(Company company);
+
+    Company getCompanyById(String id);
+
+    List<Company> getAllCompany();
+
+    void delete(String id);
 }
