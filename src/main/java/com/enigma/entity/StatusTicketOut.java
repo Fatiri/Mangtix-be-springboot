05cc33cc59c98a2f8c62a7d9a4c48c49156c3c88
Mike <mikesantia.6297@gmail.com>
2019-12-20 03:09:46 +0700

    [IMPRUVE][FEATURE]StatusTicketOut

--- src/main/java/com/enigma/entity/StatusTicketOut.java
+++ src/main/java/com/enigma/entity/StatusTicketOut.java
@@ -1,7 +1,12 @@
 package com.enigma.entity;
 
 public enum StatusTicketOut {
-    ON_SALE,
-    FREE;
+    ON_SALE("onSale"),
+    FREE("free");
 
+    private final String whetherSold;
+
+    StatusTicketOut(String whetherSold) {
+        this.whetherSold=whetherSold;
+    }
 }
