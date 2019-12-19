9749ed26d8623ce01717a55bac7c9e37ffe44bad
Nopalina Ayu Munthe <nopalinaayumunthe02@gmail.com>
2019-12-20 00:37:21 +0700

    [FEATURE] repository company

--- src/main/java/com/enigma/repositories/CompanyRepository.java
+++ src/main/java/com/enigma/repositories/CompanyRepository.java
@@ -1,4 +1,9 @@
 package com.enigma.repositories;
 
-public interface CompanyRepository {
-}
+import com.enigma.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+    public interface CompanyRepository extends JpaRepository<Company,String>{
+    }
