45c6cbf1e97d7be7a1fe297e2bbbafdf7d4bb9f7
Ragil Arain Sawala <arainsyawala@gmail.com>
2019-12-20 00:41:40 +0700

    [IMPROVE] constanta, moving from string constanta to event constanta

---
+++ src/main/java/com/enigma/constanta/EventConstanta.java
@@ -0,0 +1,15 @@
+package com.enigma.constanta;
+
+public class EventConstanta {
+
+    public static final String ID_EVENT_NOT_FOUND= "id Event %s Not Found";
+    public static final String EVENT_LOCATION_AND_DATE_TIME_CANNOT_SAME = "Sorry location and that time was already exist";
+    public static final String EVENT_TABLE = "mst_event";
+    public static final String EVENT_DETAIL_TABLE = "mst_event_detail";
+    public static final String EVENT_DATE_PATTERN = "dd-MM-yyyy";
+    public static final String EVENT_ID_RELATION = "event_id";
+    public static final String LOCATION_ID_RELATION = "location_id";
+    public static final String EVENT_MAPPED_BY = "event";
+    public static final String FILE_DIRECTORY = "/var/www/html/image/";
+
+}
