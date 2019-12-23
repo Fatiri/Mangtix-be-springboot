cb7580d3050b61c7eafa1c1f9372ba904371d947
fatiri <ilhamfatiriblog@gmail.com>
2019-12-23 15:23:20 +0700

    [Feature][improve] create ChatController

--- src/main/java/com/enigma/controller/ChatController.java
+++ src/main/java/com/enigma/controller/ChatController.java
@@ -1,4 +1,53 @@
 package com.enigma.controller;
 
+import com.enigma.entity.Chat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ChatController {
+
+
+    /*-------------------- Group (Public) chat--------------------*/
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/pubic")
+    public Chat sendMessage(@Payload Chat chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/addUser")
+    @SendTo("/topic/pubic")
+    public Chat addUser(@Payload Chat chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add user in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+
+    /*--------------------Private chat--------------------*/
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/sendPrivateMessage")
+    //@SendTo("/queue/reply")
+    public void sendPrivateMessage(@Payload Chat chatMessage) {
+        simpMessagingTemplate.convertAndSendToUser(
+                chatMessage.getReceiver().trim(), "/reply", chatMessage);
+        //return chatMessage;
+    }
+
+    @MessageMapping("/addPrivateUser")
+    @SendTo("/queue/reply")
+    public Chat addPrivateUser(@Payload Chat chatMessage,
+                                      SimpMessageHeaderAccessor headerAccessor) {
+        // Add user in web socket session
+        headerAccessor.getSessionAttributes().put("private-username", chatMessage.getSender());
+        return chatMessage;
+    }
 }
