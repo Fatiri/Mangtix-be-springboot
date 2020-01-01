49124e59ad83f5d1240ca2a3b26a928efc1b792a
Mike <mikesantia.6297@gmail.com>
2020-01-01 22:17:26 +0700

    [IMPROVE][FEATURE] search cart by user

--- src/main/java/com/enigma/services/impl/CartServiceImpl.java
+++ src/main/java/com/enigma/services/impl/CartServiceImpl.java
@@ -2,9 +2,13 @@ package com.enigma.services.impl;
 
 import com.enigma.constanta.MessageConstant;
 import com.enigma.entity.Cart;
+import com.enigma.entity.Ticket;
+import com.enigma.entity.User;
 import com.enigma.exception.NotFoundException;
 import com.enigma.repositories.CartRepository;
 import com.enigma.services.CartService;
+import com.enigma.services.TicketService;
+import com.enigma.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,9 +21,16 @@ public class CartServiceImpl implements CartService {
 
     @Autowired
     CartRepository cartRepository;
-
+    @Autowired
+    UserService userService;
+    @Autowired
+    TicketService ticketService;
     @Override
     public Cart saveCart(Cart cart) {
+        User user = userService.getUserById(cart.getUserIdTransient());
+        cart.setUser(user);
+        Ticket ticket= ticketService.getTicketById(cart.getTicketIdTransient());
+        cart.setTicket(ticket);
         return cartRepository.save(cart);
     }
 
@@ -44,5 +55,11 @@ public class CartServiceImpl implements CartService {
     @Override
     public void deleteCartById(String id) {
         cartRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Cart> getCartsByUser(String userId){
+        User user = userService.getUserById(userId);
+        return cartRepository.findCartsByUser(user);
     }
 }
